fe16bf6922eee24d23340da7a41e9b800679588d
aiden <aiden795@gmail.com>
2025-07-14 14:34:42 -0500

    Type hints, comments

--- drawing/draw.py
+++ drawing/draw.py
@@ -1,19 +1,28 @@
 import pygame
-from pygame.locals import *
 import math
-from config import *
 
 
-def thick_aaline(display, color, point0, point1, w):
+def thick_aaline(
+    display: pygame.Surface,
+    color: tuple[int, int, int],
+    point0: tuple[int, int],
+    point1: tuple[int, int],
+    w: int,
+) -> None:
+    # Unpack individual coordinates
     x0, y0 = point0
     x1, y1 = point1
+
+    # Find the distance between them
     dx = x1 - x0
     dy = y1 - y0
     distance = math.hypot(dx, dy)
 
+    # If the line has no length, there's nothing to draw
     if distance == 0:
         return
 
+    # Adjust the coordinates to create a rectangle
     proportion = w / distance / 2
     adjx = dx * proportion
     adjy = dy * proportion
@@ -22,8 +31,9 @@ def thick_aaline(display, color, point0, point1, w):
         (x0 - adjy, y0 + adjx),  # A
         (x0 + adjy, y0 - adjx),  # B
         (x1 + adjy, y1 - adjx),  # C
-        (x1 - adjy, y1 + adjx),
-    )  # D
+        (x1 - adjy, y1 + adjx),  # D
+    )
 
+    # Draw the rectangle
     pygame.draw.aalines(display, color, True, pts, True)
     pygame.draw.polygon(display, color, pts, 0)
