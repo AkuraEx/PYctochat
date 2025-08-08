038b513e1f295058817782db06b20e45ce591c89
Andrew :) <Acradawg@gmail.com>
2025-08-07 21:11:55 -0500

    awesome screenshots

--- network.py
+++ network.py
@@ -96,7 +96,7 @@ async def write_data(stream: INetStream) -> None:
 async def run(port: int, destination: str) -> None:
     # Listening Address
     # Set to 0.0.0.0 (all interfaces) at the moment
-    listen_addr = multiaddr.Multiaddr(f"/ip4/0.0.0.0/tcp/{port}")
+    listen_addr = multiaddr.Multiaddr(f"/ip4/127.0.0.1/tcp/{port}")
     host = new_host()
 
     async def stream_handler(stream: INetStream) -> None:
